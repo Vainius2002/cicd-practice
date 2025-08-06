7214d59a8b1b53580f062b41bb3ad28594eedfdf
Vainius2002 <vainius.lunys123@gmail.com>
2025-08-06 21:23:38 +0000

    Added about page

--- simple_flask.py
+++ simple_flask.py
@@ -8,5 +8,9 @@ app = Flask(__name__)
 def hello():
     return "hello world!"
 
+@app.route("/about")
+def about():
+    return "this is the about page"
+
 if __name__ == "__main__":
     app.run(debug=True)
